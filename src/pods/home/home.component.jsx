d0cbcb26a660af488212d25d32ce2f027d067a23
salino3 <programacionflavio@gmail.com>
2024-02-16 12:59:47 +0100

    add form inside details

--- src/pods/home/home.component.jsx
+++ src/pods/home/home.component.jsx
@@ -4,11 +4,14 @@ import './home.styles.css';
 export const Home = () => {
 
     return (
-        <div className="rootHome">
-            <h1 className='titleHome'>
-                Home Page
-            </h1>
-            <FormNewProduct />
-        </div>
-    )
+      <div className="rootHome">
+        <h1 className="titleHome">Home Page</h1>
+        <details className="firstdetails">
+          <summary>
+            <span>Form new Product</span>
+          </summary>
+          <FormNewProduct />
+        </details>
+      </div>
+    );
 }
