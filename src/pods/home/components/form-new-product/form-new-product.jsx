f467d02ad3d6321471a19c4a604e89f8f023c7dd
salino3 <programacionflavio@gmail.com>
2024-02-16 09:12:34 +0100

    add button form

--- src/pods/home/components/form-new-product/form-new-product.jsx
+++ src/pods/home/components/form-new-product/form-new-product.jsx
@@ -1,4 +1,5 @@
 import { FormField } from "@/common-app";
+import './form-new-product.styles.css';
 
 export const FormNewProduct = () => {
   return (
@@ -10,9 +11,46 @@ export const FormNewProduct = () => {
             txtLabel="Name"
             idName="name"
             handleChange={() => {}}
-            //  value={''}
+            value={""}
             pl="Name Product.."
           />
+          <FormField
+            type="number"
+            txtLabel="Quantity"
+            idName="quantity"
+            handleChange={() => {}}
+            value={""}
+            pl="Quantity.."
+          />
+          <FormField
+            type="text"
+            txtLabel="Code"
+            idName="code"
+            handleChange={() => {}}
+            value={""}
+            pl="Code Product.."
+          />
+          <FormField
+            type="number"
+            txtLabel="Price"
+            idName="price"
+            handleChange={() => {}}
+            value={""}
+            pl="Pice.."
+          />
+          <FormField
+            type="text"
+            txtLabel="Company"
+            idName="company"
+            handleChange={() => {}}
+            value={""}
+            pl="Company.."
+          />
+          <div>
+            <button className="btnForm" type="submit">
+                Send
+            </button>
+          </div>
         </form>
       </div>
     </div>
